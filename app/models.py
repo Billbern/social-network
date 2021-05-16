01686bceba47e9e82e3fceabcccaedcf156cd33f
Billbern <denis.abaidoo@yahoo.com>
2021-05-16 01:46:42 +0000

    resolved model password value too long

--- app/models.py
+++ app/models.py
@@ -16,8 +16,8 @@ class User(db.Model):
 
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=False, unique=True, nullable=False)
-    password = db.Column(db.String(100), index=False, unique=True, nullable=False)
+    username = db.Column(db.String(100), index=False, unique=True, nullable=False)
+    password = db.Column(db.String(200), index=False, unique=True, nullable=False)
     profile = db.relationship('Profile', backref='metadata', uselist=False)
     createdAt = db.Column(db.DateTime, default=datetime.utcnow)
     updatedAt = db.Column(db.DateTime)
@@ -28,9 +28,9 @@ class User(db.Model):
 class Profile(db.Model):
     __tablename__ = 'profile'
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(96), nullable=False)
-    lastname = db.Column(db.String(96), nullable=False)
-    email = db.Column(db.String(80), index=True, unique=True, nullable=False)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), index=True, unique=True, nullable=False)
     isOnline = db.Column(db.Boolean)
     user = db.Column(db.Integer, db.ForeignKey('user.id'))
     image = db.relationship('Image', backref='picture')
